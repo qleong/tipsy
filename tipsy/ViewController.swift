557e640ab9860069398ab81ef34fdbbaa8f7f894
Quintin Leong <qleong.dev@gmail.com>
2017-02-26 15:31:29 -0800

    Tip calculator view complete

--- tipsy/ViewController.swift
+++ tipsy/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    var tipPercentages: [Double] = []
+    static let DEFAULT_ONE_KEY = "default_percentage_one"
+    static let DEFAULT_TWO_KEY = "default_percentage_two"
+    static let DEFAULT_THREE_KEY = "default_percentage_three"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Retrieve segmented control values from user settings.
+        setupSegmentedControl()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func setupSegmentedControl() {
+        let defaults = UserDefaults.standard
+        
+        // Gets the user tip percentages, or uses default values if they do not exist.
+        tipPercentages.append(defaults.object(forKey: ViewController.DEFAULT_ONE_KEY) != nil ?
+            defaults.object(forKey: ViewController.DEFAULT_ONE_KEY)! as! Double : 0.18)
+        tipPercentages.append(defaults.object(forKey: ViewController.DEFAULT_TWO_KEY) != nil ?
+            defaults.object(forKey: ViewController.DEFAULT_TWO_KEY)! as! Double : 0.2)
+        tipPercentages.append(defaults.object(forKey: ViewController.DEFAULT_THREE_KEY) != nil ?
+            defaults.object(forKey: ViewController.DEFAULT_THREE_KEY)! as! Double : 0.25)
+    }
+    
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
