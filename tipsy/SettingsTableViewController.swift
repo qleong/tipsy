bd0dfefe5b66878358ba069ade3e7d77ea537069
Quintin Leong <qleong.dev@gmail.com>
2017-02-26 16:40:14 -0800

    Add picker with percentages as values

--- tipsy/SettingsTableViewController.swift
+++ tipsy/SettingsTableViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class SettingsTableViewController: UITableViewController {
+class SettingsTableViewController: UITableViewController, UIPickerViewDataSource,UIPickerViewDelegate {
 
+    @IBOutlet weak var tipPercentagePicker: UIPickerView!
+    @IBOutlet weak var tipSelectView: UIView!
+    var userTipPercentages: [Int] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        // Setup the uiPicker
+        setupTipPercentages()
+        tipSelectView.viewWithTag(0)?.isHidden = true
+        tipPercentagePicker.dataSource = self
+        tipPercentagePicker.delegate = self
+        
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -24,8 +36,12 @@ class SettingsTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
+    
+    func setupTipPercentages() {
+        for percentage in 1...100 {
+            userTipPercentages.append(percentage)
+        }
+    }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // One section in settings
@@ -39,15 +55,28 @@ class SettingsTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.section == 0 && indexPath.row == 0 {
-            print("Cell 1 picked")
+            tipSelectView.viewWithTag(0)?.isHidden = false
             tableView.deselectRow(at: indexPath, animated: true)
         } else if indexPath.section == 0 && indexPath.row == 1 {
-            print("Cell 2 picked")
+            tipSelectView.viewWithTag(0)?.isHidden = false
             tableView.deselectRow(at: indexPath, animated: true)
         } else if indexPath.section == 0 && indexPath.row == 2 {
-            print("Cell 3 picked")
+            tipSelectView.viewWithTag(0)?.isHidden = false
             tableView.deselectRow(at: indexPath, animated: true)
         }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 100
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return String(format: "%d%%", userTipPercentages[row])
     }
 
 }
